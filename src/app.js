5cd9eeb79abd815060a703211757af2400c6094e
iamBishwa <bishwa1996@gmail.com>
2020-11-17 19:11:12 +0530

    Finish

--- src/app.js
+++ src/app.js
@@ -8,38 +8,34 @@ app.set("view engine", "ejs");
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index");
 });
 
-// app.get("/about", (req, res) => {
-//     res.render("about");
-//   });
+app.get("/form", (req, res) => {
+  res.render("form");
+});
 
-app.post(
-  "",
-  urlencodedParser,
-  [
-    check("username", "The max-length should be 32")
-      .exists()
-      .isString()
-      .isLength({ max: 32 }),
-    check("email", "Email is not valid").isEmail().normalizeEmail(),
-    check("password", "no special characters")
-      .isLength({ min: 4 }, { max: 8 })
-      .matches(/^(?!=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, "i"),
-    check("age", "should be greater than 21").isLength({ min: 21 }),
-  ],
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const alert = errors.array();
-      res.render("", {
-        alert
-      });
-    }
+app.post("/form", urlencodedParser, [
+ check('username', 'max length should be 32')
+ .isString()
+ .isLength({max:32}),
+ check('email', 'Email is not valid')
+ .isEmail()
+ .normalizeEmail(),
+ check('password', 'password should be between 4 and 8')
+ .isLength({min:4,max:8})
+ .matches="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}", 
+ check('age', 'min should be 21')
+ .isNumeric({min:21})
+
+],(req, res) => {
+  const errors= validationResult(req)
+  if(!errors.isEmpty()){
+    const alert = errors.array()
+    res.render("form",{alert})
   }
-);
+});
 
 app.listen(port, () => {
   console.log(`listening to port: ${port}`);
